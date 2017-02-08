cfb661051442770c2b8a17fc0fbbb58121fe302b
JeffNiekamp <niekamps@gmail.com>
2017-02-07 20:10:30 -0500

    www.adsecurity.biz
    Redesign of ADS website.

---
+++ www/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+ "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Test Page</title>
+
+<style type="text/css">
+body {
+	margin: 0;
+	padding: 0;
+	font: 100%/1.3 arial,helvetica,sans-serif;
+	background: #3F6A8A;
+	background-image: url(media/bluegreenmarbleLg.jpg);
+}
+#wrap {
+	width: 700px;
+	margin: 0 auto;
+	/*temporary text styles below*/
+	text-align: center;
+	font-weight: bold;
+}
+#nav {
+    min-height:80px;
+    background:#FFFFFF;
+}
+#header {
+	min-height: 40px;
+	background: #FFFFFF;
+	padding-left: 10px;
+	padding-right: 10px;
+}
+#main {
+    width:100%;/*IE6 haslayout (contain floats)*/
+    overflow:hidden;/*contain child floats in modern browsers*/
+}
+    #content {
+        float:left;
+        width:500px;
+        min-height:200px;
+        background:#FFCC00;
+    }
+    #links {
+        float:left;
+        width:200px;
+        min-height:200px;
+        background:#62C014;
+    }
+#footer {
+    min-height:50px;
+    background:#148CE2;
+}
+</style>
+<link href="css/ADScss.css" rel="stylesheet" type="text/css">
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+
+<div id="wrap">
+  <div id="nav"><img src="media/untitled.png" width="631" height="79" alt=""/></div>
+  <div id="header" class="h1" ><img src="media/top_banner.gif" alt="" width="283" height="38" align="right"/>
+  	1219 Wilmington Ave., Dayton, Ohio 45420<br>Ph: 937.293.1177 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fx: 937.293.1179</div>
+  <div id="header">
+	<div id="saturday">
+	<ul>
+	<li><a href="index.php" title="" class="current"><span>Home</span></a></li>
+	<li><a href="retail.php" title=""><span>Retail</span></a></li>
+	<li><a href="commercial.php" title=""><span>Commercial</span></a></li>
+	<li><a href="industrial.php" title=""><span>Industrial</span></a></li>
+	<li><a href="service.php" title=""><span>Service</span></a></li>
+	<li><a href="learning.php" title=""><span>Resources</span></a></li>
+	<li><a href="contact.php" title=""><span>Contact</span></a></li>
+	</ul>
+	</div>
+  </div>
+    <div id="conternt">
+		Welcome to Advanced Digital Security<br>
+        <span class="bodytxt"><img src="media/tease_risk.png" alt="" width="160" height="120" align="right"/>Advanced Digital Security systems are designed to compliment your efforts to secure the workplace environment, help prevent theft and deter vandalism by employing digital closed circuit monitoring systems that can be viewed from any location via the internet.<br>
+		Our state-of-the-art digital video recording equipment is manufactured by in house technicians using the highest quality components available to the commercial market.<br>
+        Engineered specifically for your unique needs, our user-friendly surveillance systems are available for any interior, exterior or covert application. We carry a full line of color, medium and high-resolution dome, exterior weatherproof, bulletproof, infrared and hidden cameras.<br>
+        Factory-trained technicians, skilled in tamper-proof installation, will position your system to clearly view target areas.<br>
+        Contact one of our knowledgeable security consultants today for a free risk assessment and quote on a system designed specifically for your individual or business needs.
+	  </span>
+  </div>
+ 
+  <div class="foottxt" id="header" align="center">Copyright © 2017 Advanced Digital Security<br>
+    • | <a href="index.php">Home</a> |<a href="retail.php"> Retail</a> | <a href="commercial.php">Commercial</a> | <a href="industrial.php">Industrial</a> | <a href="service.php">Service</a> | <a href="learning.php">Resources</a> | <a href="contact.php">Contact</a> | •</div>
+</div>
+ </div>
+</body>
+</html>
